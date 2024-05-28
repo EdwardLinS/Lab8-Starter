a3d28c61c608c2ab2a366860a96da76abf14de4f
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-05-27 18:18:06 -0700

    Change register call

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -56,9 +56,7 @@ function initializeServiceWorker() {
       // B3. TODO - Register './sw.js' as a service worker (The MDN article
       //            "Using Service Workers" will help you here)
 
-      navigator.serviceWorker.register("./sw.js", {
-        scope: "/"
-      })
+      navigator.serviceWorker.register("./sw.js")
 
         // B4. TODO - Once the service worker has been successfully registered, console
         //            log that it was successful.
